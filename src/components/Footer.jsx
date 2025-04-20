141191dbd2268543b406721863f3ee9b457fe7a8
Gokulraj-98 <148607497+Gokulraj-98@users.noreply.github.com>
2025-04-20 12:14:38 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
           <div className="flex-1 mb-4 md:mb-0">
             <h3 className="text-2xl font-bold mb-2">Gokulraj</h3>
             <p className="text-gray-400">
-              Assistant System Enginner at TCS, specializing in web and
+              software developer at TCS, specializing in web and
               software development.
             </p>
           </div>
